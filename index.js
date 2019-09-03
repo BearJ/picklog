b0a43c11091dc80f6fd9edded53ba8c82063b572
BearJ <bearyanjh@foxmail.com>
2019-09-03 21:49:17 +0800

    feat: 当第一个tag分组找不到tag的时候，尝试从package.json里找

--- index.js
+++ index.js
@@ -9,16 +9,36 @@ function picklog(_args) {
     latest: false, // 是否只取上一个tag后的commit
   }, _args);
   let setting;
+  let pkg;
 
   try {
     fs.accessSync(path.resolve('.picklogrc.js'));
     setting = require(path.resolve('.picklogrc.js')); // eslint-disable-line global-require,import/no-dynamic-require
+
+    fs.accessSync(path.resolve('package.json'));
+    pkg = require(path.resolve('package.json')); // eslint-disable-line global-require,import/no-dynamic-require
   } catch (err) {
     setting = defaultSetting;
+    pkg = {};
   }
 
   return new Promise((resolve) => {
     getCommits(args, setting).then((commits) => {
+      if (commits.length && !commits[0].tag) {
+        const [firstCommit] = commits;
+        if (pkg.version) {
+          const { previousTag } = firstCommit;
+
+          if (previousTag) {
+            let prefix = previousTag.match(/^([^\d]*)/);
+            if (prefix) [, prefix] = prefix;
+
+            if (pkg.version !== previousTag.replace(prefix, '')) {
+              firstCommit.tag = `${prefix}${pkg.version}`;
+            }
+          }
+        }
+      }
       resolve(setting.parse(commits));
     });
   });
